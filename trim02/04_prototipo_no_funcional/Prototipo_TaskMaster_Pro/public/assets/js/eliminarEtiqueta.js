b439e185f54a11c146144d3bc45bea48591e7d67
FGFERNAN <fgfernan2508@gmail.com>
2024-09-04 03:08:42 -0500

    Ajustes en script de editar y eliminar etiqueta

--- trim02/04_prototipo_no_funcional/Prototipo_TaskMaster_Pro/public/assets/js/eliminarEtiqueta.js
+++ trim02/04_prototipo_no_funcional/Prototipo_TaskMaster_Pro/public/assets/js/eliminarEtiqueta.js
@@ -1,8 +1,10 @@
 const eliminarEtiqueta = document.getElementById('eliminar-etiqueta');
 const etiquetasCreadas = document.getElementById('etiquetas-creadas');
+const divEditar = document.getElementById('divEditar');
 eliminarEtiqueta.addEventListener('click', () => {
     let resultado = confirm("¿Estas seguro que deseas eliminar la etiqueta?");
     if (resultado) {
+        divEditar.remove(),
         etiquetasCreadas.remove();
         alert(`La etiqueta ha sido eliminado`);
     } else {
