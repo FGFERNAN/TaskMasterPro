36c502e5494c1873a2724170883fd41d0882e12f
FGFERNAN <fgfernan2508@gmail.com>
2024-09-19 23:41:54 -0500

    a
    a

--- trim02/07_prototipo_navegable/Prototipo_TaskMaster_Pro/public/assets/js/main.js
+++ trim02/07_prototipo_navegable/Prototipo_TaskMaster_Pro/public/assets/js/main.js
@@ -1,11 +1,12 @@
-const eliminarProyecto = document.getElementById('buton-eliminar');
-const proyecto = document.getElementById('sub-menu-proyectos');
-eliminarProyecto.addEventListener('click', () => {
-    let resultado = confirm("¿Estas seguro que deseas eliminar el proyecto?");
-    if(resultado){
-        proyecto.remove();
-        alert(`El proyecto ha sido eliminado`);
-    } else{
+const eliminarProyectos = document.querySelectorAll('#buton-eliminar'); // Selecciona todos los botones de eliminar con la clase 'boton-eliminar'
+const proyectos = document.querySelectorAll('#sub-menu-proyectos'); // Selecciona todos los proyectos (sub-menús) que se pueden eliminar
 
-    }
+eliminarProyectos.forEach((boton, index) => {
+    boton.addEventListener('click', () => {
+        let resultado = confirm("¿Estás seguro que deseas eliminar el proyecto?");
+        if (resultado) {
+            proyectos[index].remove(); // Elimina el submenú correspondiente al índice del botón
+            alert(`El proyecto ha sido eliminado`);
+        }
+    });
 });
