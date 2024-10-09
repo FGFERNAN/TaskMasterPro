96137798a1bf3343eeaaf0d65dccb9dbebc8e8bd
FGFERNAN <fgfernan2508@gmail.com>
2024-10-08 21:09:25 -0500

    Avances Micro_proyecto

---
+++ trim03/06_actividades_en_clase/Actividad_III_Trimestre/validacion.js
@@ -0,0 +1,20 @@
+class validacion{
+    constructor (nombres, apellidos, tipo_documento, num_documento, fecha_nacimiento, genero, edad, color_favorito, telefono, habilidades, hobbies, correo, contraseña){
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.tipo_documento = tipo_documento;
+        this.num_documento = num_documento;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.genero = genero;
+        this.edad = edad;
+        this.color_favorito = color_favorito;
+        this.telefono = telefono;
+        this.habilidades = habilidades;
+        this.hobbies = hobbies;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+    validarNombre(){
+        
+    }
+}
