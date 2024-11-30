ef70fa73b330acf7aaee216508597cb74d089747
FGFERNAN <fgfernan2508@gmail.com>
2024-11-29 23:52:13 -0500

    Se realiza avances en conexion de front con back
    se conecta el login

--- trim03/04_prototipo_funcional/TaskMaster_Pro/frontend/src/components/IniciarSesion.vue
+++ trim03/04_prototipo_funcional/TaskMaster_Pro/frontend/src/components/IniciarSesion.vue
@@ -5,17 +5,17 @@
         <div class="text-center">
           <img src="../assets/img/logotipo.png" class="img-fluid mx-auto d-block logo1" width="400" alt="Logo" />
         </div>
-        <form @submit.prevent="handleSubmit" class="mx-auto needs-validation custom-iniciarSesion">
+        <form ref="form" @submit.prevent="handleSubmit" class="mx-auto needs-validation custom-iniciarSesion" novalidate>
           <div class="mb-3">
             <label for="email" class="form-label">Correo Electrónico</label>
             <div class="input-group mb-3 has-validation">
               <span class="input-group-text"><i class="fa-regular fa-envelope"></i></span>
               <div class="form-floating">
-                <input v-model="email" type="email" class="form-control border-input" id="email"
-                  placeholder="Correo electrónico" required />
+                <input v-model="email" type="email" class="form-control border-input"
+                  :class="{ 'is-invalid': errors.email }" id="email" placeholder="Correo electrónico" required />
                 <label for="email">Correo Electrónico</label>
                 <div class="invalid-feedback">
-                  Por favor ingrese su correo electrónico.
+                  {{ errors.email || "Correo Invalido (Por favor ingresar el correo en el siguiente formato 'xxxx@xxx.xxx')." }}
                 </div>
               </div>
             </div>
@@ -26,11 +26,12 @@
             <div class="input-group mb-3 has-validation">
               <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
               <div class="form-floating">
-                <input v-model="password" type="password" class="form-control border-input" id="password"
-                  placeholder="Contraseña" maxlength="20" minlength="8" required />
+                <input v-model="password" type="password" class="form-control border-input"
+                  :class="{ 'is-invalid': errors.password }" id="password" placeholder="Contraseña" maxlength="20"
+                  minlength="8" required />
                 <label for="password">Contraseña</label>
                 <div class="invalid-feedback">
-                  Por favor ingrese su contraseña (mínimo 8 caracteres).
+                  {{ errors.password || "Contraseña Invalida, asegurate de que cumpla los requisitos minimos (tener minimo 8 caracteres, una letra mayuscula, una letra minuscula y un caracter especial (#,$,*).)" }}
                 </div>
               </div>
             </div>
@@ -41,9 +42,7 @@
               <label class="form-check-label" for="viewPassword">Ver Contraseña</label>
             </div>
           </div>
-          <div v-if="errorMessage" class="alert alert-danger text-center">
-            {{ errorMessage }}
-          </div>
+
           <div class="d-grid gap-2 col-6 mx-auto">
             <button class="btn btn-primary mt-3 buton-iniciar-sesion" type="submit">
               Iniciar Sesión
@@ -69,22 +68,35 @@ export default {
       email: '',
       password: '',
       showPassword: false,
-      errorMessage: "",
+      errors: {},
     };
   },
   methods: {
     async handleSubmit() {
-      try{
+      const form = this.$refs.form;
+
+      // Desactiva validación automática del navegador y aplica validación manual con Bootstrap
+      if (!form.checkValidity()) {
+        form.classList.add('was-validated');
+        return;
+      }
+      this.errors = {};
+      try {
         const response = await api.post("/login", {
           email: this.email,
           password: this.password,
         });
         console.log(response.data);
         this.$router.push("/perfil-completo");
-      } catch (err) {
-        console.error(err.response);
-        this.errorMessage = 
-          err.response?.data?.message || "Error al iniciar sesión";
+      } catch (error) {
+        if (error.response && error.response.data) {
+          const serverErrors = error.response.data;
+          if (serverErrors.message === 'Password Error') {
+            this.errors.password = 'Contraseña incorrecta.';
+          } else if (serverErrors.message === 'The user not created') {
+            this.errors.email = 'El correo no está registrado.';
+          }
+        }
       }
     },
     togglePasswordVisibility() {
