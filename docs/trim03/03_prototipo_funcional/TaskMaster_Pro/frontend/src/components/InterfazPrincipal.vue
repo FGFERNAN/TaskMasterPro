767f37373bfbfd1c3c82beace98e7ea14e1e98ca
danielatrian <erikadanielatrianabustos@gmail.com>
2025-04-10 23:37:55 -0500

    ..
    ..

--- docs/trim03/03_prototipo_funcional/TaskMaster_Pro/frontend/src/components/InterfazPrincipal.vue
+++ docs/trim03/03_prototipo_funcional/TaskMaster_Pro/frontend/src/components/InterfazPrincipal.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="bg-custom">
-    <!-- Navbar for mobile -->
+    <!-- Navbar para mobile -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light d-lg-none">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu"
-          aria-controls="offcanvasMenu">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
           <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="#">Menú</a>
       </div>
     </nav>
 
-    <!-- Offcanvas for mobile menu -->
+    <!-- Menú lateral móvil -->
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasMenuLabel">Menú</h5>
@@ -20,34 +19,25 @@
       <div class="offcanvas-body">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link mi-link" href="creacionProyecto.html"><i class="fa-solid fa-folder-plus me-1"></i> Crear
-              Proyecto</a>
+            <a class="nav-link mi-link" href="creacionProyecto.html"><i class="fa-solid fa-folder-plus me-1"></i> Crear Proyecto</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link mi-link" href="plantillasProyecto.html"><i class="fa-solid fa-folder-tree me-1"></i>
-              Utilizar Plantillas de Proyecto</a>
+            <a class="nav-link mi-link" href="plantillasProyecto.html"><i class="fa-solid fa-folder-tree me-1"></i> Utilizar Plantillas</a>
           </li>
           <li class="nav-item">
             <a class="nav-link mi-link" href="misTareas.html"><i class="fa-solid fa-list-check me-1"></i> Mis Tareas</a>
           </li>
           <li class="nav-item dropdown custom-dropdown">
-            <a class="nav-link dropdown-toggle mi-link" data-bs-toggle="dropdown" href="#"><i
-                class="fa-solid fa-folder-minus me-1"></i> Proyectos</a>
+            <a class="nav-link dropdown-toggle mi-link" data-bs-toggle="dropdown" href="#"><i class="fa-solid fa-folder-minus me-1"></i> Proyectos</a>
             <ul class="dropdown-menu">
               <li>
-                <div class="d-flex align-items-center" id="sub-menu-proyectos">
-                  <a class="dropdown-item align-items-center" href="interfazProyecto.html">
-                    <i class="fa-regular fa-folder-open"></i> Veterinaria
-                  </a>
+                <div class="d-flex align-items-center">
+                  <a class="dropdown-item" href="interfazProyecto.html"><i class="fa-regular fa-folder-open"></i> Veterinaria</a>
                   <span class="ms-1">
-                    <a href="editarProyecto.html" class="link-secondary mi-link">
-                      <i class="fa-solid fa-edit"></i>
-                    </a>
+                    <a href="editarProyecto.html" class="link-secondary mi-link"><i class="fa-solid fa-edit"></i></a>
                   </span>
                   <span class="ms-2">
-                    <a id="buton-eliminar" class="link-secondary mi-link">
-                      <i class="fa-solid fa-trash me-3"></i>
-                    </a>
+                    <a class="link-secondary mi-link" id="btn-eliminar-movil"><i class="fa-solid fa-trash me-3"></i></a>
                   </span>
                 </div>
               </li>
@@ -62,39 +52,30 @@
 
     <div class="container-fluid">
       <div class="row">
+        <!-- Sidebar para escritorio -->
         <div class="col-md-3 custom-col d-none d-lg-block">
           <img src="../assets/img/logos/logotipo.png" class="logo-inicio" width="300" />
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link mi-link" href="creacionProyecto.html"><i class="fa-solid fa-folder-plus me-1"></i>
-                Crear Proyecto</a>
+              <a class="nav-link mi-link" href="creacionProyecto.html"><i class="fa-solid fa-folder-plus me-1"></i> Crear Proyecto</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link mi-link" href="plantillasProyecto.html"><i class="fa-solid fa-folder-tree me-1"></i>
-                Utilizar Plantillas de Proyecto</a>
+              <a class="nav-link mi-link" href="plantillasProyecto.html"><i class="fa-solid fa-folder-tree me-1"></i> Utilizar Plantillas</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link mi-link" href="misTareas.html"><i class="fa-solid fa-list-check me-1"></i> Mis
-                Tareas</a>
+              <a class="nav-link mi-link" href="misTareas.html"><i class="fa-solid fa-list-check me-1"></i> Mis Tareas</a>
             </li>
             <li class="nav-item dropdown custom-dropdown">
-              <a class="nav-link dropdown-toggle mi-link" data-bs-toggle="dropdown" href="#"><i
-                  class="fa-solid fa-folder-minus me-1"></i> Proyectos</a>
+              <a class="nav-link dropdown-toggle mi-link" data-bs-toggle="dropdown" href="#"><i class="fa-solid fa-folder-minus me-1"></i> Proyectos</a>
               <ul class="dropdown-menu">
                 <li>
-                  <div class="d-flex align-items-center" id="sub-menu-proyectos">
-                    <a class="dropdown-item align-items-center" href="interfazProyecto.html">
-                      <i class="fa-regular fa-folder-open"></i> Veterinaria
-                    </a>
+                  <div class="d-flex align-items-center">
+                    <a class="dropdown-item" href="interfazProyecto.html"><i class="fa-regular fa-folder-open"></i> Veterinaria</a>
                     <span class="ms-1">
-                      <a href="editarProyecto.html" class="link-secondary mi-link">
-                        <i class="fa-solid fa-edit"></i>
-                      </a>
+                      <a href="editarProyecto.html" class="link-secondary mi-link"><i class="fa-solid fa-edit"></i></a>
                     </span>
                     <span class="ms-2">
-                      <a id="buton-eliminar" class="link-secondary mi-link">
-                        <i class="fa-solid fa-trash me-3"></i>
-                      </a>
+                      <a class="link-secondary mi-link" id="btn-eliminar-escritorio"><i class="fa-solid fa-trash me-3"></i></a>
                     </span>
                   </div>
                 </li>
@@ -106,10 +87,15 @@
           </ul>
         </div>
 
+        <!-- Contenido principal -->
         <div class="col custom-col-main">
           <header>
             <div class="header d-flex justify-content-end align-items-center p-3">
               <div class="d-flex align-items-center">
+                <!-- 🔍 Lupa -->
+                <div class="input-group me-3" style="width: 50px;">
+                  <i class="fas fa-search" @click="mostrarModalBusqueda = true" style="color: #2E7060; font-size: 30px;"></i>
+                </div>
                 <a href="notificaciones.html" class="me-2"><i class="fas fa-bell"></i></a>
                 <button class="btn btn-cerrar-sesion-ip" @click="confirmarCerrarSesion">
                   <i class="fa-solid fa-right-to-bracket me-1"></i> Cerrar Sesión
@@ -118,11 +104,33 @@
               </div>
             </div>
             <div class="text-center">
-              <h3><span class="badge p-4 color-badge">¿Que Proyecto Crearemos Hoy?</span></h3>
+              <h3><span class="badge p-4 color-badge">¿Qué Proyecto Crearemos Hoy?</span></h3>
             </div>
           </header>
 
           <section>
+            <!-- 🔍 Modal de búsqueda -->
+            <div class="modal fade" :class="{ show: mostrarModalBusqueda }" tabindex="-1" v-if="mostrarModalBusqueda" style="display: block;">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Buscar Proyecto</h5>
+                    <button type="button" class="btn-close" @click="cerrarModalBusqueda"></button>
+                  </div>
+                  <div class="modal-body">
+                    <input type="text" class="form-control" v-model="busqueda" placeholder="Escribe el nombre del proyecto" />
+                    <ul class="list-group mt-3" v-if="busqueda">
+                      <li class="list-group-item" v-for="proyecto in resultadosFiltrados" :key="proyecto.id">
+                        {{ proyecto.nombre }}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-if="mostrarModalBusqueda" class="modal-backdrop fade show"></div>
+
+            <!-- Galería de proyectos -->
             <div class="container-fluid grid project-container">
               <div class="row mb-3" v-for="(fila, filaIndex) in proyectosChunked" :key="filaIndex">
                 <div class="col text-center" v-for="(proyecto, i) in fila" :key="i">
@@ -130,8 +138,7 @@
                     <img class="img-fluid" :src="proyecto.imagen" />
                   </div>
                   <div class="mt-3">
-                    <input class="form-check-input" type="radio" name="project" :id="proyecto.id"
-                      @click="irAProyecto" />
+                    <input class="form-check-input" type="radio" name="project" :id="proyecto.id" @click="irAProyecto" />
                     <label class="form-check-label label-project" :for="proyecto.id">{{ proyecto.nombre }}</label>
                   </div>
                 </div>
@@ -140,7 +147,7 @@
                 <div class="col"></div>
                 <div class="col"></div>
                 <div class="col d-flex justify-content-end">
-                  <button @click="crearProyecto" class="btn btn-primary rounded-circle buton-crear">
+                  <button @click="crearProyecto" class="btn btn-primary rounded-circle boton-crear">
                     <i class="fa-solid fa-plus"></i>
                   </button>
                 </div>
@@ -156,7 +163,6 @@
 <script setup>
 import { ref, computed } from 'vue'
 
-// ✅ Importa imágenes desde src/assets
 import imgVeterinaria from '../assets/img/proyecto_veterinaria.jpg'
 import imgFinanzas from '../assets/img/proyecto_finanzas.jpg'
 import imgSoftware from '../assets/img/proyecto_software.jpg'
@@ -164,7 +170,6 @@ import imgSocial from '../assets/img/proyecto_social.jpg'
 import imgConstruccion from '../assets/img/proyecto_construccion.jpg'
 import imgInvestigacion from '../assets/img/proyecto_ambiental.jpg'
 
-// ✅ Lista de proyectos con imágenes importadas
 const proyectos = ref([
   { id: 'project1', nombre: 'Veterinaria', imagen: imgVeterinaria },
   { id: 'project2', nombre: 'Finanzas', imagen: imgFinanzas },
@@ -174,9 +179,10 @@ const proyectos = ref([
   { id: 'project6', nombre: 'Investigación', imagen: imgInvestigacion }
 ])
 
-const proyectosChunked = computed(() => {
-  return [proyectos.value.slice(0, 3), proyectos.value.slice(3, 6)]
-})
+const proyectosChunked = computed(() => [
+  proyectos.value.slice(0, 3),
+  proyectos.value.slice(3, 6)
+])
 
 const irAProyecto = () => {
   window.location.href = 'interfazProyecto.html'
@@ -191,8 +197,23 @@ const confirmarCerrarSesion = () => {
     window.location.href = '../../public/index.html'
   }
 }
+
+// 🔍 Modal de búsqueda
+const mostrarModalBusqueda = ref(false)
+const busqueda = ref('')
+
+const cerrarModalBusqueda = () => {
+  mostrarModalBusqueda.value = false
+  busqueda.value = ''
+}
+
+const resultadosFiltrados = computed(() => {
+  return proyectos.value.filter(p =>
+    p.nombre.toLowerCase().includes(busqueda.value.toLowerCase())
+  )
+})
 </script>
 
 <style scoped>
-/* Tus estilos personalizados aquí */
+/* Puedes agregar tus estilos aquí o usar los del proyecto */
 </style>
