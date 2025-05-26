dfce077d2085c1b4a2db6259e3f08302effa11a4
FGFERNAN <fgfernan2508@gmail.com>
2025-05-26 13:54:10 -0500

    Avance backend modulo tareas

--- docs/trim03/03_prototipo_funcional/TaskMaster_Pro/backend/src/controllers/taskController.js
+++ docs/trim03/03_prototipo_funcional/TaskMaster_Pro/backend/src/controllers/taskController.js
@@ -12,7 +12,43 @@ class TaskController {
         } catch (err) {
             res.status(500).json({ message: err.message });
         }
-    }
+    };
+
+    getTaskById = async (req, res) => {
+        try {
+            const task = await this.taskService.getTaskById(req.params.id);
+            res.json({ message: "Method Get Id: Task Found", data: task, status: 200 });
+        } catch (err) {
+            res.status(404).json({ message: err.message });
+        }
+    };
+
+    createTask = async (req, res) => {
+        try {
+            const newTask = await this.taskService.createTask(req.body);
+            res.status(201).json(newTask);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    };
+
+    updateTask = async (req, res) => {
+        try {
+            const response = await this.taskService.updateTask(req.params.id, req.body);
+            res.status(200).json(response);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    };
+
+    daleteTask = async (req, res) => {
+        try {
+            const response = await this.taskService.deleteTask(req.params.id);
+            res.status(200).json(response);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    };
 }
 
 module.exports = new TaskController();
