8ea575228a5806dccbbe64ad3f3c44be74e06997
FGFERNAN <fgfernan2508@gmail.com>
2025-05-28 01:10:50 -0500

    Se arreglan estilos de interfaz crear tarea

--- docs/trim03/03_prototipo_funcional/TaskMaster_Pro/frontend/src/components/crearTarea.vue
+++ docs/trim03/03_prototipo_funcional/TaskMaster_Pro/frontend/src/components/crearTarea.vue
@@ -9,12 +9,12 @@
               <path
                 d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
             </svg>
-            <span class="button-text">Regresar</span>
+            <span class="button-text"> Regresar</span>
           </button>
           <h1 class="mb-0 text-center flex-grow-1">
             Crear Tarea
           </h1>
-          <img src="../../public/assets/img/logotipo.png" alt="Logo" class="img-fluid logo" />
+          <img src="../assets/img/logotipo.png" alt="Logo" class="logo" />
         </div>
       </div>
       <form class="container form-create-project needs-validation" novalidate @submit.prevent="validateForm">
@@ -127,6 +127,9 @@
 
 <script>
 export default {
+  mounted() {
+    document.title = "Crear Tarea | TaskMaster Pro";
+  },
   name: 'CrearTarea',
   methods: {
     goBack() {
@@ -145,7 +148,5 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
-@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css');
-@import '../../public/assets/css/style.css';
+@import "@/../../assets/css/EditarPerfil.css";
 </style>
