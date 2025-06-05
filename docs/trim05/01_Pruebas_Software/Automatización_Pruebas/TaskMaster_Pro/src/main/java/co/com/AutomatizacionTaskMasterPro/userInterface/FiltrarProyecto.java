1c796d873c5f7ee5f5616d6223658ae6385a7d7b
FGFERNAN <fgfernan2508@gmail.com>
2025-06-05 00:54:01 -0500

    Ajustes Finales

--- docs/trim05/01_Pruebas_Software/Automatización_Pruebas/TaskMaster_Pro/src/main/java/co/com/AutomatizacionTaskMasterPro/userInterface/FiltrarProyecto.java
+++ docs/trim05/01_Pruebas_Software/Automatización_Pruebas/TaskMaster_Pro/src/main/java/co/com/AutomatizacionTaskMasterPro/userInterface/FiltrarProyecto.java
@@ -5,11 +5,10 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class FiltrarProyecto {
 
-    public static Target BTN_LUPA = Target.the("").located(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/header/div[1]/div/div/i"));
+    public static Target BTN_LUPA = Target.the("Dar click en el boton de buscar").located(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/header/div[1]/div/div/i"));
 
-    public static Target INPUT_NOMBREPROYECTO = Target.the("").located(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/section/div[1]/div/div/div[2]/input"));
-    public static Target CAJA_TEXTO = Target.the("").located(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/section/div[1]/div/div/div[2]/ul/a"));
-    public static Target TEXTO_CONFIRMACION = Target.the("").located(By.xpath("//*[@id=\"7\"]/div[2]/div/div[2]/section/div[1]/div[1]/h2"));
-
+    public static Target INPUT_NOMBREPROYECTO = Target.the("Ingresar el nombre del proyecto que se va a filtrar").located(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/section/div[1]/div/div/div[2]/input"));
+    public static Target CAJA_TEXTO = Target.the("Busqueda del nombre").located(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div[2]/section/div[1]/div/div/div[2]/ul/a"));
+    public static Target TEXTO_CONFIRMACION = Target.the("Confirmacion del proyecto filtrado").located(By.xpath("//*[@id=\"7\"]/div[2]/div/div[2]/section/div[1]/div[1]/h2"));
 
 }
